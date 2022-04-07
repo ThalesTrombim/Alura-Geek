4758456a86e562a5d01c25dcf7fb84afddccd670
ThalesTrombim <thalestr@outlook.com>
2022-04-06 21:23:13 -0300

    feat: se a nova categoria não existir insere no banco, recupa info do produto a ser editado

--- pages/products/edit/[id].jsx
+++ pages/products/edit/[id].jsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 import style from './style.module.scss';
@@ -11,54 +11,114 @@ import { NextHead } from '../../../src/components/Head';
 export default function EditProduct({ product, categories }) {
     const { register, handleSubmit } = useForm()
     const { handleSetDropDown } = useContext(DropDownContext)
+    const [ editedProduct, setEditedProduct ] = useState(product);
+
+    async function categoryId(category){
+        const nameCategory = `${category[0].toUpperCase()}${category.slice(1)}`
+        const categoryExisting = categories.find(el => {
+            return(
+                el.name ===  nameCategory
+            )
+        });
+        
+        if(categoryExisting){
+            return (categoryExisting.id)
+        }
+
+        const { data, error } = await supabaseClient
+        .from('categories')
+        .insert([
+          { name: nameCategory }
+        ])
+
+        return data[0].id
+    }
+
+    console.log(editedProduct)
 
     async function updateProduct(){
+        const teste = await categoryId(editedProduct.categories.name)
+
+        console.log('teste', teste)
+        // const { data, error } = await supabaseClient
+        // .from('products')
+        // .update({ 
+        //     name: product.name,
+        //     category: 
+        // })
+        // .match({ name: 'Auckland' })
+
 
     }
 
-    console.log(product.img)
-    console.log(categories)
     return (
+
+        // <div>teste</div>
         <div className={style.editContainer} onClick={handleSetDropDown}>
             <NextHead>Cadastrar produto</NextHead>
             <form onSubmit={handleSubmit(updateProduct)} className={style.formContainer}>
                 <h2>Editar produto</h2>
-                <div>
-                    {/* <Image src={product.img} alt="Alura Geek" width={'560px'} height={'403px'} /> */}
-
+                <Image src={product.img} alt="Alura Geek" width={'250px'} height={'303px'} />
+                <main className={style.main}>
                     <div 
                         className={style.dropFile} 
                         onDrop={(e) => dropHandler(e)}
                         onDragOver={(event) => { event.preventDefault()}}
                         name='local_image'
                         style={{
-                            backgroundImage: `url(${product.img})`, 
+                            // backgroundImage: `url(${img})`, 
                             backgroundPosition: 'center center',
                             backgroundSize: 'cover',
                             backgroundRepeat: 'no-repeat',
                         }}
                     >
-.
+                        <Image src={'/icons/input-file.png'} width={'32px'} height={'32px'} alt='Arraste para adicionar uma imagem para o produto'/>
+                        <p>Arraste para adicionar uma imagem para o produto</p>
                     </div>
+                    
                     Ou
                     <label htmlFor="input_file" className={style.labelInputFile}>
                         Procure no seu computador
                     </label>
                     <input name='local_image' type="file" id='input_file' style={{display: 'none'}}/>
-                </div>
+                </main>
 
                 <div className={style.inputsArea}>
                     <div className={style.inputDefault}>
                         <label htmlFor="product_name">
                             Nome do produto
                         </label>
-                        <input className='cleanForm' id='product_name' type="text" name='name' {...register('name')}/>
+                        <input 
+                            value={editedProduct.name} 
+                            className='cleanForm' 
+                            id='product_name' 
+                            type="text" 
+                            name='name' 
+                            {...register('name')}
+                            onChange={e => {
+                                setEditedProduct(
+                                    {...editedProduct, name: e.target.value}
+                                )
+                            }}
+                        />
                     </div>
                     <div className={style.inputDefault}>
                         <label htmlFor="product_price">
                             Preço do produto
                         </label>
-                        <input className='cleanForm' id='product_price' type="text" name='price' {...register('price')}/>
+                        <input 
+                            value={editedProduct.price} 
+                            className='cleanForm' 
+                            id='product_price' 
+                            type="text" 
+                            name='price' 
+                            {...register('price')}
+                            onChange={e => {
+                                setEditedProduct(
+                                    {...editedProduct, price: e.target.value}
+                                )
+                            }}
+                        />
                     </div>
                     <div className={style.inputDefault}>
                         <label htmlFor="product_category">
@@ -72,13 +132,40 @@ export default function EditProduct({ product, categories }) {
                                 ))
                             }
                         </datalist>
-                        <input list="suggestions" className='cleanForm' id='product_category' type="text" name='category' {...register('category')}/>
+                        <input 
+                            value={editedProduct.categories.name} 
+                            list="suggestions" 
+                            className='cleanForm' 
+                            id='product_category' 
+                            type="text" 
+                            name='category' 
+                            {...register('category')}
+                            onChange={e => {
+                                setEditedProduct(
+                                    {...editedProduct, categories: { name: e.target.value } }
+                                )
+                            }}
+                        />
                     </div>
                     <div className={style.textarea}>
                         <label htmlFor="product_desc">
                             Descrição do produto
                         </label>
-                        <textarea className='cleanForm' maxLength={230} id='product_desc' name='desc' {...register('desc')}></textarea>
+                        <textarea 
+                            value={editedProduct.desc} 
+                            className='cleanForm' 
+                            maxLength={230} 
+                            id='product_desc' 
+                            name='desc' 
+                            {...register('desc')}
+                            onChange={e => {
+                                setEditedProduct(
+                                    {...editedProduct, desc: e.target.value}
+                                )
+                            }}
+                        >
+
+                        </textarea>
                     </div>
                 </div>
                 <button type='submit'>Adicionar produto</button>
@@ -90,7 +177,10 @@ export default function EditProduct({ product, categories }) {
 export async function getServerSideProps(context) {
     const id = context.params.id
 
-    const { data } = await supabaseClient.from('products').select().eq('id', id)
+    const { data } = await supabaseClient.from('products')
+    .select(`*, categories ( name )`)
+    .eq('id', id)
+
     const categoryList = await supabaseClient.from('categories').select()
 
     const product = data[0]
